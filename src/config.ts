0e354c562ede81ebe6bd89857f5eae420e1dc9ae
glnmario <glnmario.work@gmail.com>
2026-04-11 17:48:31 +0100

    Update site description

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Mario Giulianelli';
-export const SITE_DESCRIPTION = 'Associate Professor of Computational Linguistics at UCL working on language, cognition, interaction, and AI evaluation.';
+export const SITE_DESCRIPTION = 'Associate Professor of Computational Linguistics at UCL working on language and information processing in humans and AI systems, and on AI evaluation and interpretability.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
